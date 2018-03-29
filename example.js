5d2c95a2309b2f3902efcba9fd16ebd9afc53335
Nir Alfasi <alfasin@gmail.com>
2018-03-29 01:58:48 -0700

    remove logger

--- example.js
+++ example.js
@@ -1,10 +1,9 @@
 // Example of usage
 const smartyStreets = require('SmartyStreetsAsPromised');
-const logger = require('node-logger').createLogger();
     
 exports.address = function (req, res, next) {
     const {street, city, state} = req.query;
-    logger.info(`request for ${street} ${city} ${state}`);
+    console.log(`request for ${street} ${city} ${state}`);
     return smartyStreets.address(req.query)
         .then(result => res.send(result))
         .catch(err => res.status(err.code || 404).send(err.message));
