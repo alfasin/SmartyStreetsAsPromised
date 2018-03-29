66483462298684a86bf25fcc8af7d4a9d5d492ed
Nir Alfasi <alfasin@gmail.com>
2018-03-29 01:28:51 -0700

    moving code to standard 2-spaces alignment

--- smarty_streets.js
+++ smarty_streets.js
@@ -7,20 +7,23 @@ let smartyStreetsInstance;
 
 // This is exported for testing
 exports.SmartyStreetsApi = () => {
-    if (!smartyStreetsInstance) {
-        const SmartyStreets = require('smartystreets-api');
-        smartyStreetsInstance = SmartyStreets(SMARTY_STREETS_CREDS_ID, SMARTY_STREETS_CREDS_TOKEN);
-        smartyStreetsInstance.addressPromised = Promise.promisify(smartyStreetsInstance.address, {context: smartyStreetsInstance, multiArgs: true});
-    }
-    return smartyStreetsInstance;
+  if (!smartyStreetsInstance) {
+    const SmartyStreets = require('smartystreets-api');
+    smartyStreetsInstance = SmartyStreets(SMARTY_STREETS_CREDS_ID, SMARTY_STREETS_CREDS_TOKEN);
+    smartyStreetsInstance.addressPromised = Promise.promisify(smartyStreetsInstance.address, {
+      context: smartyStreetsInstance,
+      multiArgs: true
+    });
+  }
+  return smartyStreetsInstance;
 };
 
 // That's the function you want to use
 exports.address = address => {
-    return exports.SmartyStreetsApi().addressPromised(address).spread((data, raw) => {
-        // here you can process the response from smarty-streets
-        // you may choose to return either data, raw or both. 
-        // raw contains the full details while data is a smaller digested version
-        return raw; 
-    });
+  return exports.SmartyStreetsApi().addressPromised(address).spread((data, raw) => {
+    // here you can process the response from smarty-streets
+    // you may choose to return either data, raw or both. 
+    // raw contains the full details while data is a smaller digested version
+    return raw;
+  });
 };
