c53c4ca991b18e23879c4a3066685690fc798b07
Nir Alfasi <alfasin@Nirs-MacBook-Pro.local>
2018-03-31 18:59:55 -0700

    hide original impl. and call address directly. add express route example + usage

--- smarty_streets.js
+++ smarty_streets.js
@@ -2,26 +2,15 @@ const Promise = require('bluebird');
 
 let smartyStreetsInstance;
 
-// This is exported for testing
-exports.SmartyStreetsApi = () => {
+module.exports = () => {
   if (!smartyStreetsInstance) {
     const SmartyStreets = require('smartystreets-api');
     // make sure your env contains your smarty-streets id and token!
     smartyStreetsInstance = SmartyStreets(process.env.SMARTY_STREETS_CREDS_ID, process.env.SMARTY_STREETS_CREDS_TOKEN);
-    smartyStreetsInstance.addressPromised = Promise.promisify(smartyStreetsInstance.address, {
+    smartyStreetsInstance.address = Promise.promisify(smartyStreetsInstance.address, {
       context: smartyStreetsInstance,
       multiArgs: true
     });
   }
   return smartyStreetsInstance;
-};
-
-// That's the function you want to use
-exports.address = address => {
-  return exports.SmartyStreetsApi().addressPromised(address).spread((data, raw) => {
-    // here you can process the response from smarty-streets
-    // you may choose to return either data, raw or both. 
-    // raw contains the full details while data is a smaller digested version
-    return raw;
-  });
 };
